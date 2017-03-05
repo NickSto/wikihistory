cbab3ce52db43bc61e8aea1be3148486af70d47c
Stephen LaPorte <stephen.laporte@gmail.com>
2017-03-04 19:34:38 -0800

    grouping edits by day

--- server.py
+++ server.py
@@ -1,12 +1,13 @@
 from flask import Flask, jsonify
+from collections import Counter
 import graph
 
 app = Flask(__name__)
 
 @app.route('/edits/<username>')
 def get_edits(username):
-
     data = graph.get_edits(username);
-    data = [d for d in data]
-    json = jsonify(data)
+    data = [d['timestamp'].split('T')[0] for d in data]
+    counts = Counter(data)
+    json = jsonify(counts)
     return json
