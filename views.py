098bcbc4968328306aa4ad5385a26a14ee39e898
Nick <nmapsy@gmail.com>
2017-03-15 12:47:20 -0700

    views.py: Actually, don't log hits to the API as user traffic.

--- views.py
+++ views.py
@@ -1,11 +1,6 @@
 from django.http import HttpResponse
 import json
 from . import wikiedits
-try:
-  from traffic.lib import add_visit
-except ImportError:
-  def add_visit(request, response):
-    return response
 
 
 def edits_list(request, user):
@@ -14,7 +9,7 @@ def edits_list(request, user):
   limit = params.get('limit')
   if limit:
     limit = int(limit)
-  response = add_visit(request, HttpResponse(content_type='text/plain; charset=utf-8'))
+  response = HttpResponse(content_type='text/plain; charset=utf-8')
   edits = []
   for edit in wikiedits.get_edits(user, limit):
     if format == 'human':
@@ -30,13 +25,13 @@ def edits_per_day(request, user):
   date_counts = {}
   for date, count in wikiedits.get_edits_per_day(user, time_limit=365):
     date_counts[date] = count
-  response = add_visit(request, HttpResponse(content_type='application/json; charset=utf-8'))
+  response = HttpResponse(content_type='application/json; charset=utf-8')
   json.dump(date_counts, response)
   return response
 
 
 def day_edits(request, user, date):
   edits = list(wikiedits.get_edits_for_day(user, date))
-  response = add_visit(request, HttpResponse(content_type='application/json; charset=utf-8'))
+  response = HttpResponse(content_type='application/json; charset=utf-8')
   json.dump(edits, response)
   return response
