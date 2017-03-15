03e73f5b9ac5973e64a6a63f03c05de10e1dd208
Nick <nmapsy@gmail.com>
2017-03-15 09:14:22 -0700

    {views,urls}.py: Add Django code for necessary APIs.

--- urls.py
+++ urls.py
@@ -4,5 +4,7 @@ from . import views
 
 app_name = 'wikihistory'
 urlpatterns = [
-  url(r'^(?P<user>.+)$', views.view, name='view'),
+  url(r'^edits_list/(?P<user>.+)$', views.edits_list, name='edits_list'),
+  url(r'^edits_per_day/(?P<user>.+)$', views.edits_per_day, name='edits_per_day'),
+  url(r'^day_edits/(?P<user>.+)/(?P<date>.+)$', views.day_edits, name='day_edits'),
 ]
--- views.py
+++ views.py
@@ -1,21 +1,42 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 import json
-from . import graph
+from . import wikiedits
+try:
+  from traffic.lib import add_visit
+except ImportError:
+  def add_visit(request, response):
+    return response
 
-def view(request, user):
+
+def edits_list(request, user):
   params = request.GET
   format = params.get('format', 'human')
   limit = params.get('limit')
   if limit:
     limit = int(limit)
-  response = HttpResponse(content_type='text/plain; charset=utf-8')
+  response = add_visit(request, HttpResponse(content_type='text/plain; charset=utf-8'))
   edits = []
-  for edit in graph.get_edits(user, limit):
+  for edit in wikiedits.get_edits(user, limit):
     if format == 'human':
       response.write('{}\t{}\n'.format(edit['timestamp'], edit['title']))
     elif format == 'json':
       edits.append(edit)
   if format == 'json':
     json.dump(edits, response)
+  return response
+
+
+def edits_per_day(request, user):
+  date_counts = {}
+  for date, count in wikiedits.get_edits_per_day(user, time_limit=365):
+    date_counts[date] = count
+  response = add_visit(request, HttpResponse(content_type='application/json; charset=utf-8'))
+  json.dump(date_counts, response)
+  return response
+
+
+def day_edits(request, user, date):
+  edits = list(wikiedits.get_edits_for_day(user, date))
+  response = add_visit(request, HttpResponse(content_type='application/json; charset=utf-8'))
+  json.dump(edits, response)
   return response
