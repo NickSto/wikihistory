0d7fe410d3073286ea27e354b306708c92c8eb7b
Nick <git@mailship.org>
2024-04-20 00:26:32 -0400

    urls.py: Update for Django 4.x compatibility.

--- urls.py
+++ urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from . import views
 
 app_name = 'wikihistory'
 urlpatterns = [
-  url(r'^edits_list/(?P<user>.+)$', views.edits_list, name='edits_list'),
-  url(r'^edits_per_day/(?P<user>.+)$', views.edits_per_day, name='edits_per_day'),
-  url(r'^day_edits/(?P<user>.+)/(?P<date>.+)$', views.day_edits, name='day_edits'),
+  re_path(r'^edits_list/(?P<user>.+)$', views.edits_list, name='edits_list'),
+  re_path(r'^edits_per_day/(?P<user>.+)$', views.edits_per_day, name='edits_per_day'),
+  re_path(r'^day_edits/(?P<user>.+)/(?P<date>.+)$', views.day_edits, name='day_edits'),
 ]
